4539d94784317d21240d4b5858f0c8e63eca4e12
Muhammad Taha <mohamedengineer944@gmail.com>
2021-05-25 16:27:39 +0200

    Adding Mongo Atlas URL to the project for database connection

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const multer = require("multer");
+const usersRouter = require("./routes/users");
+const ordersRouter = require("./routes/orders");
+const productRouter = require("./routes/products");
+const cartsRoutser = require("./routes/carts");
 var cors = require("cors");
 const app = express();
 
+const mongoURL = "mongodb+srv://mohamed-admin:mohamed-admin@cluster0.9hgm2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 const port = 4001;
 
+mongoose
+    .connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch((err) => console.log("Failed to connect to Mongodb,", err.message));
 
 app.use(express.static("./uploads/users"));
 app.use(express.static("./uploads/products"));
@@ -14,9 +28,9 @@ app.use(express.static("./uploads/logo"));
 app.use(express.static("./uploads/slider"));
 
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
 
 app.use(cors());
@@ -27,4 +41,4 @@ app.use("/orders", ordersRouter);
 app.use("/users", usersRouter);
 app.use("/carts", cartsRoutser);
 
-app.listen(port, () => console.log(`Server listens on port ${port}`));
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
