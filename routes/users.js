dc723f6fec9ec7094b27631a588fa2d206ca4589
aya-maher <ayamaherali9@gmail.com>
2021-05-25 17:14:47 +0200

    Sign Up

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Cart = require("../models/cart");
+const validateUser = require("../helpers/validateuser");
 
 const multer = require("multer");
 var storage = multer.diskStorage({
@@ -19,3 +23,66 @@ router.get("/", async (req, res) => {
   const users = await User.find({});
   res.send(users);
 });
+
+//signup method
+
+router.post("/signup", async (req, res) => {
+    const { error } = validateUser(req.body);
+    if (error) {
+      return res.status(400).send(error.details);
+    }
+    try {
+      const { email, userName, password, gender, isAdmin, image } = req.body;
+  
+      const userExists = await User.findOne({
+        email,
+      });
+  
+      if (userExists) return res.status(400).send("email already exist");
+      let user = new User({
+        userName,
+        email,
+        gender,
+        isAdmin,
+        password,
+        image,
+      });
+  
+      let cart = new Cart({
+        userId: user._id,
+        productsList: [],
+      });
+      user.cart = cart._id;
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      await cart.save();
+      const payload = {
+        user: {
+          id: user._id,
+          userName: user.userName,
+          isAdmin: user.isAdmin,
+        },
+      };
+      jwt.sign(
+        payload,
+        "secret",
+        {
+          expiresIn: "24h",
+        },
+        (err, token) => {
+          if (err) return res.status(500).send("token creation error");
+          return res
+            .status(200)
+            .json({
+              token,
+              user,
+            })
+            .send();
+        }
+      );
+    } catch (error) {
+      return res.status(500).send("seving error");
+    }
+  });
+  
