ef15426064b8525e03dd65d4c8a975861167f9fb
Muhammad Taha <mohamedengineer944@gmail.com>
2021-05-25 16:38:53 +0200

    Search for single product

--- routes/products.js
+++ routes/products.js
@@ -14,4 +14,77 @@ router.get("/", async (req, res) => {
     res.send(products);
 });
 
+
+//search for product
+router.post("/search", async (req, res) => {
+    const searchparams = {
+        ...req.body,
+    };
+    if (searchparams.title) {
+        await Product.find(
+            {
+                $and: [
+                    {
+                        title: new RegExp(".*" + searchparams.title + ".*"), //TODO : case sensitive???
+                    },
+                    {
+                        isDeleted: { $ne: true },
+                    },
+                ],
+            },
+            (error, result) => {
+                if (error) {
+                    return res.send(error);
+                }
+                return res.send(result);
+            }
+        );
+    } else if (searchparams.Brand) {
+        await Product.find(
+            {
+                $and: [
+                    {
+                        "details.Brand": new RegExp(".*" + searchparams.Brand + ".*"), //TODO : case sensitive???
+                    },
+                    {
+                        isDeleted: { $ne: true },
+                    },
+                ],
+            },
+            (error, result) => {
+                if (error) {
+                    return res.send(error);
+                }
+                return res.send(result);
+            }
+        );
+    } else if (searchparams.Processor) {
+        await Product.find(
+            {
+                $and: [
+                    {
+                        "details.Processor": new RegExp(
+                            ".*" + searchparams.Processor + ".*"
+                        ), //TODO : case sensitive???
+                    },
+                    {
+                        isDeleted: { $ne: true },
+                    },
+                ],
+            },
+            (error, result) => {
+                if (error) {
+                    return res.send(error);
+                }
+                return res.send(result);
+            }
+        );
+    } else {
+        return res.status(404).send("No match found");
+    }
+    return res.status(404).send("No data sent for search");
+});
+
+
+
 module.exports = router;
