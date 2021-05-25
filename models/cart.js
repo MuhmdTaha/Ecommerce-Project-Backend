b599865ebd31b87c9078ac63943aad4c602f0a58
mohamed-elbermawy <mohamed77bermawy@gmail>
2021-05-25 17:41:34 +0200

    implementing cart model

---
+++ models/cart.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+
+  productsList: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Product",
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = Cart;
