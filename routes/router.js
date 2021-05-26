3087bd4084efdc65660ed2ab066de6bc7441f001
Ahmed-behiery <ahmedbehiery96@gmail.com>
2021-05-26 20:17:47 +0200

    view user orders

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const Order = require("../models/order");
+const CheckToken = require("../midlware/auth");
+
 
 
 
@@ -11,5 +13,21 @@ const Order = require("../models/order");
 router.get("/", async (req, res) => {
     const orders = await Order.find({});
     if (!orders) return res.status(404).send("No orders found");
+    res.send(orders);
+  });
+
+  //view user orders
+router.get("/user/:id", CheckToken, async (req, res) => {
+    const id = req.params.id;
+    const { error } = validateObjectId(id);
+    if (error) {
+      return res.status(400).send("Invalid ID");
+    }
+    const orders = await Order.find({
+      user: id,
+    });
+    if (!orders) {
+      return res.status(400).send("No orders found");
+    }
     res.send(orders);
   });
